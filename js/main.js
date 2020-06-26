aac0861dcd87584da13e8563af5ff7ad267b657a
Dennis Oba <DennisOba@Denniss-iMac-3.local>
2020-06-26 10:37:03 -0400

    made opacities more consistent

--- js/main.js
+++ js/main.js
@@ -46,18 +46,18 @@ function exit(){
     var tl4 = new TimelineMax()
 
     tl
-        .to("#f-path2", 4, {opacity: .8, drawSVG: "100% 100%", ease: ease})
+        .to("#f-path2", 4, {opacity: .9, drawSVG: "100% 100%", ease: ease})
         TweenMax.to("#f-path2", 2, {strokeWidth: 36, delay: 1})
 
     tl4 
-        .to("#f-path1", 3, {opacity: .8, drawSVG: "100% 100%", ease: ease, delay: .8})
+        .to("#f-path1", 3, {opacity: .9, drawSVG: "100% 100%", ease: ease, delay: .8})
         // .to("#f-path2", 1.8, {opacity: 1, drawSVG: "0% 100%", ease: ease, delay: 2.6}, "sync+=.2")
 
     tl2
-        .to("#i-path", 3, {opacity: .8, drawSVG: "100% 100%", ease: ease, delay: .8})
+        .to("#i-path", 3, {opacity: .9, drawSVG: "100% 100%", ease: ease, delay: .8})
 
     tl3
-        .to("#la-path", 3, {opacity: .8, drawSVG: "0% 0%", ease: ease, delay: .4})
+        .to("#la-path", 3, {opacity: .95, drawSVG: "0% 0%", ease: ease, delay: .4})
 
         TweenMax.to("#la-path", 1, {strokeWidth: 36, delay: .4})
         TweenMax.to("#la-path, #f-path1, #f-path2, #i-path", 1, {opacity: 0, delay: 1.6, onComplete: clearProps})
@@ -83,7 +83,7 @@ function init(){
 function set(){
     TweenMax.set("#f-path1, #f-path2, #i-path, #la-path", {opacity: .7, drawSVG: "0 0"})
     TweenMax.set("#i-path", {opacity: .7, drawSVG: "0 0"})
-    TweenMax.set("#la-path", {opacity: .7, drawSVG: "0% 0%", onComplete: start})
+    TweenMax.set("#la-path", {opacity: .75, drawSVG: "0% 0%", onComplete: start})
 }
 
 init()
